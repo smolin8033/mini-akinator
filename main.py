7c64e5722e9336bf686282153dabedc446c99362
Александр Смолин <smolll123@mail.ru>
2021-03-01 13:43:31 +0300

    Creating the list of animals

---
+++ main.py
@@ -0,0 +1,5 @@
+# You choose one animal from the list. The program will ask the questions about it,
+# you can answer only "Yes" or "No". The program should guess and win.
+
+animals = ['Cow', 'Mouse', 'Camel', 'Hawk', 'Cat', 'Dog', 'Piranha', 'Shark', 'Butterfly',
+           'Bee', 'Mosquito', 'Fly', 'Snow Leopard', 'Chicken', 'Duck']
