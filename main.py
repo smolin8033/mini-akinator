23ddae4512580cf949c1efd959d6169711c4531f
Александр Смолин <smolll123@mail.ru>
2021-03-01 13:49:05 +0300

    Creating a list of questions

--- main.py
+++ main.py
@@ -3,3 +3,20 @@
 
 animals = ['Cow', 'Mouse', 'Camel', 'Hawk', 'Cat', 'Dog', 'Piranha', 'Shark', 'Butterfly',
            'Bee', 'Mosquito', 'Fly', 'Snow Leopard', 'Chicken', 'Duck']
+
+q = [
+    'Is your animal longer than a Macbook Air? Pls google, if in doubt.\n(a) Yes\n(b) No\n\n',
+    'Can your animal be domestic? Do people keep it on a farm/ in their homes?\n(a) Yes\n(b) No\n\n',
+    'Do people keep it for its meat, milk or eggs?\n(a) Yes\n(b) No\n\n',
+    'Do people keep it for its skin?\n(a) Yes\n(b) No\n\n',
+    'Can it fly? Even a little?\n(a) Yes\n(b) No\n\n',
+    'Does it like Pedigree or Chappi more than Whiskas and Kitekat?\n(a) Yes\n(b) No\n\n',
+    'Does it swim in the water?\n(a) Yes\n(b) No\n\n',
+    'Does it hunt alone?\n(a) Yes\n(b) No\n\n',
+    'Can it fly like a king of the sky?\n(a) Yes\n(b) No\n\n',
+    'Can it survive the ardent heat?\n(a) Yes\n(b) No\n\n',
+    'Can it sting a person, even if it does it seldom?\n(a) Yes\n(b) No\n\n',
+    'Does it collect honey?\n(a) Yes\n(b) No\n\n',
+    'Do you want to kill it during the sleepless night?\n(a) Yes\n(b) No\n\n',
+    'Can it fly above the lake?\n(a) Yes\n(b) No\n\n',
+]
